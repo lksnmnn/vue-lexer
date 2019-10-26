7359b947cf219bcca3d59f3db35d4025345581eb
Lukas Neumann <lksnmnn@gmail.com>
2019-10-26 09:46:17 +0200

    Add tests remove closing >

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -92,6 +92,22 @@ class VueLexerTestCase(TestCase):
           (Token.Punctuation, '>')
         ])
     
+    def test_lexing_directive_two_with_accessors(self):
+        lexer = lexers.get_lexer_by_name('vue')
+        tokens = lexer.get_tokens('''
+            <path v-for="link in graph.links" :key="link.id">
+        ''')
+
+        self.assertEqual (self.__filter_tokens (tokens), [
+          (Token.Punctuation, '<'),
+          (Token.Name.Tag, 'path'),
+          (Token.Name.Tag, 'v-for'),
+          (Token.Literal.String, '="link in graph.links" '),
+          (Token.Name.Tag, ':key'),
+          (Token.Literal.String, '="link.id"'),
+          (Token.Punctuation, '>')
+        ])
+
     def test_lexing_directive_two_with_accessors_and_forward_slash(self):
         lexer = lexers.get_lexer_by_name('vue')
         tokens = lexer.get_tokens('''
@@ -105,7 +121,21 @@ class VueLexerTestCase(TestCase):
           (Token.Literal.String, '="link in graph.links" '),
           (Token.Name.Tag, ':key'),
           (Token.Literal.String, '="link.id"'),
+          (Token.Punctuation, '/'),
           (Token.Punctuation, '>')
+        ])
+
+    def test_trailing_forward_slash(self):
+        lexer = lexers.get_lexer_by_name('vue')
+        tokens = lexer.get_tokens('''
+            <path />
+        ''')
+
+        self.assertEqual (self.__filter_tokens (tokens), [
+          (Token.Punctuation, '<'),
+          (Token.Name.Tag, 'path'),
+          (Token.Punctuation, '/'),
+          (Token.Punctuation, '>'),
         ])
 
     def test_lexing_directive_three(self):
