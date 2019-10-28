d7b8c2687e904d272b89f8ab507ac552d33541a2
Lukas Neumann <lksnmnn@gmail.com>
2019-10-28 18:32:34 +0100

    Linting

--- vue/lexer.py
+++ vue/lexer.py
@@ -2,36 +2,45 @@ import re
 
 from pygments.lexer import bygroups, default, include
 from pygments.lexers.javascript import JavascriptLexer
-from pygments.token import Literal, Name, Operator, Punctuation, String, Text
+from pygments.token import Name, Operator, Punctuation, String, Text
 
 # Use same tokens as `JavascriptLexer`, but with tags and attributes support
 TOKENS = JavascriptLexer.tokens
 
-TOKENS.update({
-    'vue': [
-        (r'(<)([\w-]+)', bygroups(Punctuation, Name.Tag), 'tag'),
-        (r'(<)(/)([\w]+)(>)', bygroups(Punctuation, Punctuation, Name.Tag, Punctuation)),
-    ],
-    'tag': [
-        (r'\s+', Text),
-        (r"([@:]?[\w-]+\s*)(=)(\s*)", bygroups(Name.Attribute, Operator, Text), "attr"),
-        (r'[{}]+', Punctuation),
-        (r'[\w\.-]+', Name.Attribute),
-        (r'(/?)(\s*)(>)', bygroups(Punctuation, Text, Punctuation), '#pop'),
-    ],
-    'attr': [
-        ('{', Punctuation, 'expression'),
-        ('".*?"', String, '#pop'),
-        ("'.*?'", String, '#pop'),
-        default ('#pop')
-    ],
-    'expression': [
-        ('{', Punctuation, '#push'),
-        ('}', Punctuation, '#pop'),
-        include('root')
-    ]
-})
-TOKENS['root'].insert(0, include('vue'))
+TOKENS.update(
+    {
+        "vue": [
+            (r"(<)([\w-]+)", bygroups(Punctuation, Name.Tag), "tag"),
+            (
+                r"(<)(/)([\w]+)(>)",
+                bygroups(Punctuation, Punctuation, Name.Tag, Punctuation),
+            ),
+        ],
+        "tag": [
+            (r"\s+", Text),
+            (
+                r"([@:]?[\w-]+\s*)(=)(\s*)",
+                bygroups(Name.Attribute, Operator, Text),
+                "attr",
+            ),
+            (r"[{}]+", Punctuation),
+            (r"[\w\.-]+", Name.Attribute),
+            (r"(/?)(\s*)(>)", bygroups(Punctuation, Text, Punctuation), "#pop"),
+        ],
+        "attr": [
+            ("{", Punctuation, "expression"),
+            ('".*?"', String, "#pop"),
+            ("'.*?'", String, "#pop"),
+            default("#pop"),
+        ],
+        "expression": [
+            ("{", Punctuation, "#push"),
+            ("}", Punctuation, "#pop"),
+            include("root"),
+        ],
+    }
+)
+TOKENS["root"].insert(0, include("vue"))
 
 
 class VueLexer(JavascriptLexer):
